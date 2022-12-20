70c686e910f325b75e8ff4e7cf48a20bd4e6020b
Vu Van Minh <min6th@gmail.com>
2022-12-20 17:58:35 +0700

    test with secret

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -4,25 +4,23 @@ import { Client } from "./../index";
 describe("Client unit test", () => {
 	let config = {
 		...new Config(),
-		clientId: "",
-		secret: "",
+		clientId: "796de824-97c8-48ce-ba50-75a3a4ea085e",
+		secret: "6WWJEU53klJerZLlyfKXdoycEpaU8xTZNkElOvKu9Nui288WhCkuhUsMTMHang0B",
 		host: "localhost:9666",
 		ssl: false,
 		apiVersion: "v2",
 	};
 	const client = new Client(config);
-	let aCategories, naCategories;
+	let response1, response2;
 	test("Get all categories async", async () => {
-		aCategories = await client.async_getCategories();
+		response1 = await client.async_getCategories();
 	});
 	test("Get all categories no_async", async () => {
 		await client.noAsync_getCategories().then((res: AxiosResponse) => {
-			naCategories = res.data;
+			response2 = res.data;
 		});
-		expect(aCategories).toEqual(naCategories);
+		expect(response1).toEqual(response2);
 	});
 
-	test("Get all anime by category async", async () => {
-
-    });
+	test("Get all anime by category async", async () => {});
 });
